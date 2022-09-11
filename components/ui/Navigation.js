8880c082ffcc6ac95d8ee1a55d2da3a4cab779c8
KML <kaungmyatlin.worklife@gmail.com>
2022-09-12 01:09:58 +0630

    init

---
+++ components/ui/Navigation.js
@@ -0,0 +1,9 @@
+import { Fragment } from "react"
+
+function Navigation(props) {
+    return (
+        <Fragment>{props.children}</Fragment>
+    )
+}
+
+export default Navigation
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import Navigation from '../components/ui/Navigation'
+import classes from '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className={classes.app}>
+      <Navigation>
+        <Component {...pageProps} />
+      </Navigation>
+    </div>
+  )
 }
 
 export default MyApp
